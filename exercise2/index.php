300b5dcc483fdc73ce96826365b64b3488cbcf64
Naqeeb <naqeeblat@gmail.com>
2022-10-11 21:51:29 +0800

    exercise2

---
+++ exercise2/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Webpage title</title>
+</head>
+
+<body>
+
+Web
+<!--This is a comment-->
+
+page content goes  here
+
+</body>
+</html>
