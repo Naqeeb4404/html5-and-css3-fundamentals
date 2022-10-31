3bfb8f43f5c4eb5eb06ad159438f38afe3942de8
Naqeeb <naqeeblat@gmail.com>
2022-10-31 21:13:03 +0800

    exercise4

---
+++ exercise4/page2/add folder/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Links</title>
+<style>
+    a:link{
+        color:red;
+        text-decoration:none;
+    }
+    a:visited{
+        color:green;
+    }
+    a:hover{
+        color:blue
+    }
+    nav ui{
+        list-style-type:none;
+    }
+    nav li{
+        display:inline;
+    }
+    nav a:link, nav a:visited{
+        background-color-red;
+        color:white;
+    }
+    nav a:hover{
+        background-color:blue;
+    }
+</head>
+
+<body>
+
+<h1>Links</h1>
+
+<h2>Internal link</h2>
+linked page
+
+<h2>External link</h2>
+http://www.wikipedia.org/
+
+<br/><br/>
+<img src="golden-gate-sm.jpg" alt="the golden gate bridge" width="500" height="213" />
+
+<h1>Navigation Links</h1>
+<ul>
+<li>Link 1</li>
+<li>Link 2</li>
+<li>Link 3</li>
+</ul>
+
+
+</body>
+</html>
